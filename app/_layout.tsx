f185a262f0cb08451d3cde90d77c41bacd5ea036
Jiyeon-Kang <kang158632@gmail.com>
2025-01-23 20:09:18 +0900

    feat: add about screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,17 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+      <Stack>
+        {/*
+        [What is Stack?]
+        A stack navigator is the foundation for navigating between different screens in an app.
+        On Android, a stacked route animates on top of the current screen.
+        On iOS, a stacked route animates from the right.
+        Expo Router provides a Stack component to create a navigation stack to add new routes.
+        */}
+        <Stack.Screen name={"index"} options={{title:'Home'}}/>
+        <Stack.Screen name={"about"} options={{title:'About'}}/>
+      </Stack>
+  );
 }
