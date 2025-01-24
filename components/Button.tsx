b16c964e921faf2509323789de4930d1b8096b68
Jiyeon-Kang <kang158632@gmail.com>
2025-01-24 18:35:52 +0900

    feat: add props for reusable button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,11 +1,32 @@
 // {Pressable} can detect single taps, long presses, trigger separate events when the button is pushed in and released, and more.
 import { StyleSheet, View, Pressable, Text } from 'react-native';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 type Props = {
     label: string;
+    theme?: 'primary';
 };
 
-export default function Button({ label }: Props) {
+export default function Button({ label, theme }: Props) {
+    //Choose a photo button
+    if (theme === 'primary') {
+        return (
+            <View
+                style={[
+                    styles.buttonContainer,
+                    { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 },
+                ]}>
+                <Pressable
+                    style={[styles.button, { backgroundColor: '#fff' }]}
+                    onPress={() => alert('You pressed a button.')}>
+                    <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
+                    <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
+                </Pressable>
+            </View>
+        );
+    }
+
+    //Use this photo button
     return (
         <View style={styles.buttonContainer}>
             <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
@@ -31,6 +52,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         flexDirection: 'row',
+    },
+    buttonIcon: {
+        paddingRight: 8,
     },
     buttonLabel: {
         color: '#fff',
