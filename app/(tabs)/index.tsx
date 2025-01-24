ae53e25fdbbb2e505a551743ae2aed9ac5ed54fa
Jiyeon-Kang <kang158632@gmail.com>
2025-01-24 17:53:59 +0900

    feat: add button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import { View, StyleSheet } from "react-native";
 
+import Button from '@/components/Button';
 import ImageViewer from "@/components/ImageViewer";
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
@@ -9,6 +10,10 @@ export default function Index() {
         <View style={styles.container}>
             <View style={styles.imageContainer}>
                 <ImageViewer imgSource={PlaceholderImage} />
+            </View>
+            <View style={styles.footerContainer}>
+                <Button label="Choose a photo" />
+                <Button label="Use this photo" />
             </View>
         </View>
     );
@@ -23,9 +28,8 @@ const styles = StyleSheet.create({
     imageContainer: {
         flex: 1,
     },
-    image: {
-        width: 320,
-        height: 440,
-        borderRadius: 18,
+    footerContainer: {
+        flex: 1 / 3,
+        alignItems: 'center',
     },
 });
