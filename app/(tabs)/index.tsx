c5138455d0e9834e40052d6e1a778fe2b2ff4891
Jiyeon-Kang <kang158632@gmail.com>
2025-01-24 12:48:02 +0900

    fix: fix file path of Link

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ export default function Index() {
   return (
       <View style={styles.container}>
           <Text style={styles.text}>About screen</Text>
-          <Link href="/my-app/app/(tabs)/about" style={styles.button}>
+          <Link href="/about" style={styles.button}>
               Go to About screen
           </Link>
       </View>
