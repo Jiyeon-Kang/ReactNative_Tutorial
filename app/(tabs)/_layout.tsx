b8d7b8a405ab3dd7f37771797f5d0bc3a737a803
Jiyeon-Kang <kang158632@gmail.com>
2025-01-24 12:59:42 +0900

    feat: update button tab navigator appearance

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,21 @@
 import { Tabs } from "expo-router";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function RootLayout() {
   return (
-      <Tabs>
+      <Tabs
+          screenOptions={{
+              tabBarActiveTintColor: '#ffd33d',
+              headerStyle: {
+                  backgroundColor: '#25292e',
+              },
+              headerShadowVisible: false,
+              headerTintColor: '#fff',
+              tabBarStyle: {
+                  backgroundColor: '#25292e',
+              },
+          }}
+      >
         {/*
         [What is Stack?]
         A stack navigator is the foundation for navigating between different screens in an app.
@@ -10,8 +23,24 @@ export default function RootLayout() {
         On iOS, a stacked route animates from the right.
         Expo Router provides a Stack component to create a navigation stack to add new routes.
         */}
-        <Tabs.Screen name={"index"} options={{title:'Home'}}/>
-        <Tabs.Screen name={"about"} options={{title:'About'}}/>
+        <Tabs.Screen
+            name="index"
+            options={{
+                title:'Home',
+                tabBarIcon:({color,focused})=>(
+                    <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+                )
+        }}
+        />
+          <Tabs.Screen
+              name="about"
+              options={{
+                  title: 'About',
+                  tabBarIcon: ({ color, focused }) => (
+                      <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24}/>
+                  ),
+              }}
+          />
       </Tabs>
   );
 }
